0e777dc9355105773caadefc1c3b77276fdcd45d
Gabe-Levin <glev2190@gmail.com>
2023-01-16 10:35:33 +0100

    Synchronizing job Precipitation Netcdf to Parquet from AWS Glue at 2023/01/16 09:35:33

--- jobs/1/Precipitation Netcdf to Parquet/Precipitation Netcdf to Parquet.py
+++ jobs/1/Precipitation Netcdf to Parquet/Precipitation Netcdf to Parquet.py
@@ -2,24 +2,18 @@
 import boto3
 import botocore
 import datetime
-import os.path
 import pandas as pd
 import xarray as xr
 
+# Create client for connecting ERA5 S3 directly
 era5_bucket = 'era5-pds'
-
-# AWS access / secret keys required
-# s3 = boto3.resource('s3')
-# bucket = s3.Bucket(era5_bucket)
-
-# No AWS keys required
 client = boto3.client('s3', config=botocore.client.Config(signature_version=botocore.UNSIGNED))
 
-# select date and variable of interest
+# Select date and variable of interest
 date = datetime.date(2022,5,1)
 var = 'precipitation_amount_1hour_Accumulation'
 
-# file path patterns for remote S3 objects and corresponding local file
+# File path patterns for remote S3 objects and corresponding local file
 s3_data_ptrn = '{year}/{month}/data/{var}.nc'
 data_file_ptrn = '{year}{month}_{var}.nc'
 
@@ -28,17 +22,24 @@ month = date.strftime('%m')
 s3_data_key = s3_data_ptrn.format(year=year, month=month, var=var)
 data_file = data_file_ptrn.format(year=year, month=month, var=var)
 
-if not os.path.isfile(data_file): # check if file already exists
-    print("Downloading %s from S3..." % s3_data_key)
-    client.download_file(era5_bucket, s3_data_key, data_file)
+# Extract netcdf file directly from ERA5 bucket
+client.download_file(era5_bucket, s3_data_key, data_file)
 
+# Use xarray to access the contents of the netcdf file
 ds = xr.open_dataset(data_file)
 
+# Cycle through each day of the month of May, 2022
 for day in range(1,32):
+    # Clear the filtered df
     df_filtered= pd.DataFrame()
+    
+    # Partition the data by day
     file_date = f'2022-05-{day:02d}'
     ds_filtered=ds.sel(time1=slice(file_date,file_date))
+    
+    # Assign the filtered data to a pandas df
     df_filtered = ds_filtered.to_dataframe()
-
+    
+    # Save the filtered df to s3 as a compressed parquet
     s3_url = f's3://jua-code-challenge-data/data/precipitation_data/raw_parquet_by_day/precipitation_{file_date}.parquet.gzip'
     df_filtered.to_parquet(path=s3_url, compression='gzip') 
