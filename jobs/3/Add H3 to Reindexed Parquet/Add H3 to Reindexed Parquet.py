2754d926dbbc059456c8e99a1e96141d143020c8
Gabe-Levin <glev2190@gmail.com>
2023-01-17 15:44:37 +0200

    Synchronizing job Add H3 to Reindexed Parquet from AWS Glue at 2023/01/17 13:44:37

--- jobs/3/Add H3 to Reindexed Parquet/Add H3 to Reindexed Parquet.py
+++ jobs/3/Add H3 to Reindexed Parquet/Add H3 to Reindexed Parquet.py
@@ -13,6 +13,7 @@ for day in range(1,32):
     
     # For each day, (1) add h3 to the chuncked dataframes, (2) combine them, and (3) then save the result to s3
     df_by_day = pd.DataFrame()
+    
     for df in dfs:
         # Add h3 indexing field
         df['h3_index'] = df.apply(lambda row: h3.geo_to_h3(row['lat'], row['lon'], 2), axis=1)
@@ -21,5 +22,5 @@ for day in range(1,32):
         df_by_day = df_by_day.append(df,ignore_index=True)
     
     # Save result to a different s3 directory
-    s3_url = f's3://jua-code-challenge-data/data/precipitation_data/reindexed_parquet_by_day_wH3/precipitation_{file_date}.parquet.gzip'
-    s3.to_parquet(df=df_by_day, path=s3_url, compression='gzip')
+    s3_url = f's3://jua-code-challenge-data/data/precipitation_data/reindexed_parquet_by_day_wH3/precipitation_{file_date}.parquet'
+    s3.to_parquet(df=df_by_day, path=s3_url)
